1ffe7273c69f795b84221c60479f301f45b4cb68
Yoonsuh Kim <blawesomek@gmail.com>
2024-10-23 04:31:23 +0900

    spec page 수정

--- lib/pages/spec2.dart
+++ lib/pages/spec2.dart
@@ -4,8 +4,16 @@ class spec2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: AppBar(
+        title: Text('Detail'),
+      ),
+      body: Center(
+        child: Text(
+          '상세한 설명.\n이상앱 MBTI 포부 등등',
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 }
